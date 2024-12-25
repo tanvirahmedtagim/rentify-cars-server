2d748a2afdbf6b914fc90e55979de8afb6d67891
tanvirahmedtagim <tanvirahmedtagim@gmail.com>
2024-12-25 23:15:44 +0600

    work complete

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://rentify-cars.netlify.app"],
+  origin: ["https://rentify-cars.netlify.app"],
   credentials: true,
   optionalSuccessStatus: 200,
 };
